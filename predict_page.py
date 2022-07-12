89644e406de85cd698b0e2792d474e11722b0352
A Kiran Raj <ambal.kiranraj@gmail.com>
2022-07-12 14:59:41 +0530

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -4,7 +4,7 @@ import pickle
 
 
 from PIL import Image
-image = Image.open('/home/expert/MY_END_TO_END_PROJECTS/ML_Project_Streamlit_End_TO_End_Project/large.png')
+image = Image.open('/home/expert/MY_END_TO_END_PROJECTS/ML_Project_Streamlit_End_TO_End_Project/large.jpeg')
 
 
 #LOAD MODEL FROM PICKLE
